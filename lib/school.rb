b9c89205b4e9b830e9a4e1f2da63862f493a1371
Jason Crescenzo <crescenzojay@gmail.com>
2018-10-31 21:21:39 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
-# code here!
+class School 
+
+def initialize(school_name)
+  @school_name= school_name
+  @roster = {}
+end
+
+def roster
+  @roster
+end
+
+def add_student(student, grade)
+ @student = student
+ @grade = grade
+ if @roster.include?(grade)== false 
+   @roster[grade] = []
+ end 
+ @roster[grade]<< student
+end
+ 
+  def grade(num)
+    
+def sort 
+  @roster.each {|k,v|
+  v.sort!}
+end
+end
+
